d2cf9271b037ea5090e0834b84a803487006d75e
gsajeendra <43973552+sajeendragavi@users.noreply.github.com>
2021-04-24 23:44:41 +0300

    created database file

---
+++ Backend/functions/databaseschemas.js
@@ -0,0 +1,63 @@
+let db = {
+    users: [
+      {
+        userId: 'dh23ggj5h32g543j5gf43',
+        email: 'user@email.com',
+        createdAt: '2019-03-15T10:59:52.798Z',
+        imageUrl: 'image/dsfsdkfghskdfgs/dgfdhfgdh',
+        bio: 'Hello, my name is user, nice to meet you',
+        location: 'Lonodn, UK'
+      }
+    ],
+    courses: [
+      {
+        child_subject: "Artificial intelligence",
+        course_id:303,
+        course_name:"Introduction to artificial intelligence",
+        length: 10,
+        next_session_date:"",
+        parent_subject:"Computer Science",
+        provider:"Udacity",
+        universities: "Stanford university",
+        url: "https://www.ai-class.com/",
+        video_url:"https://www.youtube.com/watch?feature=player_embedded&v=BnIJ7Ba5Sr4"
+      }
+    ],
+   
+  };
+  const userDetails = {
+    // Redux data
+    credentials: {
+      userId: 'N43KJ5H43KJHREW4J5H3JWMERHB',
+      email: 'user@email.com',
+      createdAt: '2019-03-15T10:59:52.798Z',
+      imageUrl: 'image/dsfsdkfghskdfgs/dgfdhfgdh',
+      bio: 'Hello, my name is user, I am a student',
+      location: 'Lonodn, UK'
+    },
+    likes: [
+      {
+        userEmail: 'user',
+        course_id: 'hh7O5oWfWucVzGbHH2pa'
+      },
+      {
+        userEmail: 'user',
+        course_id: 'hh7O5oWfWucVzGbHH3pa'
+      }
+    ],
+
+    favJobs: [
+      {
+        userEmail: 'user@email.com',
+        jobTitle: 'hh7O5oWfWucVzGbHH2pa',
+        jobLink:'https://www.esco-job.com/'
+      },
+      {
+        userEmail: 'user@email.com',
+        jobTitle: 'hh7O5oWfWucVzGbHH2pa',
+        jobLink:'https://www.esco-job.com/'
+      }
+    ]
+
+
+  };
