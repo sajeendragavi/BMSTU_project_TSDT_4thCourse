644473e4ba601cdb18bea889ff7b6ac3a522316b
hamyy2912 <74626106+hamyy2912@users.noreply.github.com>
2021-04-24 23:57:13 +0300

    Upload config and fbAuth

---
+++ Backend/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "APIKEY",
+    authDomain: "AUTHDOMAIN",
+    databaseURL: "DBURL",
+    projectId: "PROJECTID",
+    storageBucket: "STORAGEBUCKET",
+    messagingSenderId: "MID",
+    appId: "APPID",
+    measurementId: "MEASUREMENTID"
+};
